0ff474c9f1553cd16503459573623f771498d268
MohammedSolyman <MohammedSolyman10@gmail.com>
2023-10-26 12:29:28 +0200

    relocate button was replaced

--- lib/Presentation_layer/widgets/relocate_button/relocate_button.dart
+++ lib/Presentation_layer/widgets/relocate_button/relocate_button.dart
@@ -1,6 +1,6 @@
 import 'package:al_huda/Presentation_layer/controllers/my_animation_controller.dart';
 import 'package:al_huda/Presentation_layer/controllers/quran_controller.dart';
-import 'package:al_huda/util/constants/paths_consts.dart';
+import 'package:al_huda/util/constants/colors_consts.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -23,10 +23,38 @@ class RelocateButton extends StatelessWidget {
             onTap: () {
               controller.relocateToCurrentAyah(headIndex);
             },
-            child: Image.asset(RelocatePaths.relocate1),
+            child: const MyRoundButton(),
           ),
         ),
       );
     });
+  }
+}
+
+class MyRoundButton extends StatelessWidget {
+  const MyRoundButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: BlueColor.blueColor.shade900,
+            width: 4,
+          ),
+          gradient: LinearGradient(colors: [
+            SeaColor.seaColorAccents.shade100,
+            SeaColor.seaColorAccents.shade200,
+            SeaColor.seaColorAccents.shade400,
+            SeaColor.seaColorAccents.shade700,
+          ]),
+          boxShadow: [
+            BoxShadow(
+                color: BlueColor.blueColor.shade900,
+                blurRadius: 5,
+                offset: const Offset(1, 1))
+          ]),
+    );
   }
 }
